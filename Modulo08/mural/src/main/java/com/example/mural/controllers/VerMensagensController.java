86971c7693389d35e49649bb2a8f79ba448fdbd0
andreendo <andreendo@gmail.com>
2025-05-16 16:40:25 -0300

    exercício para o spring - mural code atual

---
+++ Modulo08/mural/src/main/java/com/example/mural/controllers/VerMensagensController.java
@@ -0,0 +1,44 @@
+package com.example.mural.controllers;
+
+import com.example.mural.dto.ListedMessage;
+import com.example.mural.repositories.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class VerMensagensController {
+
+    private final Logger logger = LoggerFactory.getLogger(VerMensagensController.class);
+
+    private MessageRepository messageRepository;
+
+    public VerMensagensController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @GetMapping("/mensagens")
+    public String verMensagens(Model model) {
+        logger.info("GET /mensagens");
+
+        var listedMessages = new ArrayList<ListedMessage>();
+        messageRepository.getMessages().forEach(m -> {
+            listedMessages.add(new ListedMessage(
+                    m.getFrom(),
+                    m.getTo(),
+                    m.getMessage(),
+                    m.getTimestamp()
+            ));
+        });
+
+        model.addAttribute("messages", listedMessages);
+        logger.info("Listed messages: {}", listedMessages);
+
+        return "mensagens";
+    }
+
+}
